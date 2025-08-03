581a343ca6d51df5a2c8aa4259abe4af3c96009d
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-08-03 17:40:21 +0200

    Add RequestLoggingFilter for request duration logging and update application properties for Swagger UI path

---
+++ src/main/java/com/manga/server/filtres/RequestLoggingFilter.java
@@ -0,0 +1,32 @@
+package com.manga.server.filtres;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.log4j.Log4j2;
+
+@Component
+@Log4j2
+public class RequestLoggingFilter extends OncePerRequestFilter {
+
+  @Override
+  protected void doFilterInternal(HttpServletRequest request,
+      HttpServletResponse response,
+      FilterChain filterChain) throws ServletException, IOException {
+
+    long startTime = System.currentTimeMillis();
+
+    try {
+      filterChain.doFilter(request, response);
+    } finally {
+      long duration = System.currentTimeMillis() - startTime;
+      log.info("{} {} - {} ms", request.getMethod(), request.getRequestURI(), duration);
+    }
+  }
+}
