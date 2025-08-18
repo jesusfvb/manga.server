a5fc9eabb4fb44a8140032895822ee34fe696261
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-08-18 18:21:06 +0200

    Method searchManga update to consume the BD.

--- src/main/java/com/manga/server/services/MangaService.java
+++ src/main/java/com/manga/server/services/MangaService.java
@@ -7,6 +7,7 @@ import com.manga.server.scrapers.Scraper;
 import lombok.AllArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -25,46 +26,69 @@ public class MangaService {
         if (newListMangaService.isTimeCheck(scrapper)) {
             var mangas = leerCapituloScraper.getNewChapter();
             exitOfSave(mangas);
-            newListMangaService.saveListNewMangas(mangas,scrapper);
-            log.info("Is for:" + scrapper);
+            newListMangaService.saveListNewMangas(mangas, scrapper);
+            log.info("Is new mangas for:" + scrapper);
             return mangas;
         } else {
-            log.info("Is for: data base" );
+            log.info("Is new mangas for: data base");
             return newListMangaService.getLastListNewManga(scrapper);
         }
     }
 
     public List<MangaModel> searchManga(String query) {
-        var mangas = leerCapituloScraper.searchMangas(query);
-        exitOfSave(mangas);
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("name",
+                ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase());
+
+        var example = Example.of(MangaModel.builder().name(query).build(), matcher);
+        var mangas = mangaRepository.findAll(example);
+        var logMessage = "Is search for: data base";
+
+//        TODO Actualizar este numero en un futuro
+        if (mangas.size() <= 3) {
+            var mangasScrapper = leerCapituloScraper.searchMangas(query);
+            for (var magaScraper : mangasScrapper) {
+                if(mangas.stream().noneMatch(m-> m.getName().equals(magaScraper.getName()))){
+                    exitOfSave(magaScraper);
+                    mangas.add(magaScraper);
+                    logMessage = "Is search for:" +leerCapituloScraper.toString();
+                };
+            }
+        }
+        log.info(logMessage);
         return mangas;
     }
 
-    public String mangaDescription(String id){
+    public String mangaDescription(String id) {
         var mangaOptional = mangaRepository.findById(id);
-        if(mangaOptional.isPresent()){
-         var manga = mangaOptional.get();
-         if(manga.getDescription()== null){
-           var description =  leerCapituloScraper.getMangaDescription(manga.getUrl());
-           manga.setDescription(description);
-           mangaRepository.save(manga);
-           return  description;
-         }
-         return  manga.getDescription();
+        if (mangaOptional.isPresent()) {
+            var manga = mangaOptional.get();
+            if (manga.getDescription() == null) {
+                var description = leerCapituloScraper.getMangaDescription(manga.getUrl());
+                manga.setDescription(description);
+                mangaRepository.save(manga);
+                log.info("Is description for:" + ScrappersEnum.leerCapitulo);
+                return description;
+            }
+            log.info("Is description for: data base");
+            return manga.getDescription();
         }
-        return  null;
+        return null;
+    }
+
+    private void exitOfSave(MangaModel manga){
+        var example = MangaModel.builder().name(manga.getName()).url(manga.getUrl()).build();
+        var exit = mangaRepository.findOne(Example.of(example));
+        if (exit.isEmpty()) {
+            var id = mangaRepository.save(manga).getId();
+            manga.setId(id);
+        } else {
+            manga.setId(exit.get().getId());
+        }
     }
 
     private void exitOfSave(List<MangaModel> listManga) {
         for (var manga : listManga) {
-            var exmaple = MangaModel.builder().name(manga.getName()).url(manga.getUrl()).build();
-            var exit = mangaRepository.findOne(Example.of(exmaple));
-            if (exit.isEmpty()) {
-                var id = mangaRepository.save(manga).getId();
-                manga.setId(id);
-            } else {
-                manga.setId(exit.get().getId());
-            }
+            exitOfSave(manga);
         }
     }
 }
