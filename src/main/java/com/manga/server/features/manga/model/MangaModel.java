a50a1cb1ae5d0b4d6fb4628682538882b01e2655
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-10-11 21:35:03 +0200

    fix(manga): Fix problem whit Transient in lastChapter variable

--- src/main/java/com/manga/server/features/manga/model/MangaModel.java
+++ src/main/java/com/manga/server/features/manga/model/MangaModel.java
@@ -1,28 +1,39 @@
 package com.manga.server.features.manga.model;
 
 import org.springframework.data.annotation.Id;
+import org.springframework.data.annotation.Transient;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @Document(collection = "manga")
 public class MangaModel {
 
-  @Id
-  private String id;
+        @Id
+        private String id;
 
-  @Indexed(unique = true)
-  private String name;
+        @Indexed(unique = true)
+        private String name;
 
-  private String url;
+        private String url;
 
-  private String thumbnail;
+        private String thumbnail;
 
-  private Double numberOfChapters;
+        private String description;
 
-  private String description;
+        private LocalDateTime lastUpdated;
+
+        @Transient
+        @Builder.Default
+        private Double lastChapter = null;
 }
