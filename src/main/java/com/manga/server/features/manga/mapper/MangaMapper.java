4a9fa016cff8e5191aa9bbd63fd53edde3b22e02
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-10-14 17:05:11 +0200

    refactor(MangaMapper): remove unnecessary methods

--- src/main/java/com/manga/server/features/manga/mapper/MangaMapper.java
+++ src/main/java/com/manga/server/features/manga/mapper/MangaMapper.java
@@ -5,7 +5,6 @@ import java.util.List;
 import org.mapstruct.Mapper;
 
 import com.manga.server.features.manga.dtos.MangaDTO;
-import com.manga.server.features.manga.dtos.NewMangaDTO;
 import com.manga.server.features.manga.dtos.SearchMangaDTO;
 import com.manga.server.features.manga.model.MangaModel;
 
@@ -15,10 +14,6 @@ public interface MangaMapper {
   List<MangaDTO> mangasToMangaDTOs(List<MangaModel> mangas);
 
   MangaDTO mangaToMangaDTO(MangaModel manga);
-
-  List<NewMangaDTO> mangasToNewMangaDTOs(List<MangaModel> mangas);
-
-  NewMangaDTO mangaToNewMangaDTO(MangaModel manga);
 
   SearchMangaDTO mangaToSearchMangaDTO(MangaModel manga);
 
