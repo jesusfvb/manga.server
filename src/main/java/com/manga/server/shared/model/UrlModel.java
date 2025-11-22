18e8a8abb097114bfdb046113ffcbc5ede638b46
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-11-22 16:55:37 +0100

    feat(model): add UrlModel class to encapsulate URL and scrapper information for improved structure

---
+++ src/main/java/com/manga/server/shared/model/UrlModel.java
@@ -0,0 +1,13 @@
+package com.manga.server.shared.model;
+
+import com.manga.server.shared.enums.ScrappersEnum;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UrlModel {
+    private String url;
+    private ScrappersEnum scrapper;
+}
