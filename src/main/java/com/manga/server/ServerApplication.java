9a63335cf4306904b0a2dd0b4097136fc78feb54
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2025-11-23 20:21:07 +0100

    refactor(application): remove CommandLineRunner from ServerApplication and create StartRunner for application startup logic

--- src/main/java/com/manga/server/ServerApplication.java
+++ src/main/java/com/manga/server/ServerApplication.java
@@ -1,25 +1,14 @@
 package com.manga.server;
 
-import com.manga.server.features.manga.services.MangaService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @EnableAsync
 @SpringBootApplication
-public class ServerApplication implements CommandLineRunner {
+public class ServerApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ServerApplication.class, args);
   }
-
-  @Autowired
-  private MangaService mangaService;
-
-    @Override
-    public void run(String... args) throws Exception {
-        mangaService.starApp();
-    }
 }
