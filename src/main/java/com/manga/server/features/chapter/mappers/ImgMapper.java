2c83929daf81cb718c48cdb54912cd3c06ae48c5
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-03 21:16:52 +0100

    refactor(img): annotate ImgMapper with Spring service annotation for improved integration

--- src/main/java/com/manga/server/features/chapter/mappers/ImgMapper.java
+++ src/main/java/com/manga/server/features/chapter/mappers/ImgMapper.java
@@ -4,14 +4,15 @@ import java.util.List;
 
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.springframework.stereotype.Service;
 
 import com.manga.server.features.chapter.dtos.ImgDTO;
 import com.manga.server.features.chapter.models.ImgModel;
 
+@Service
 @Mapper(componentModel = "spring")
 public interface ImgMapper {
 
-    
     @Mapping(target = "url", source = "img.url.url")
     public ImgDTO imgModelToImgDTO(ImgModel img);
 
