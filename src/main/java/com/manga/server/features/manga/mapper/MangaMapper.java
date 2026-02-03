dbc89532bcd3e748a2e3eb087ecb828e8b15e5f3
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-03 21:16:34 +0100

    refactor(manga): update MangaMapper to use Spring service annotation and improve method naming

--- src/main/java/com/manga/server/features/manga/mapper/MangaMapper.java
+++ src/main/java/com/manga/server/features/manga/mapper/MangaMapper.java
@@ -1,19 +1,14 @@
 package com.manga.server.features.manga.mapper;
 
-import java.util.List;
-
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-
 import com.manga.server.features.manga.model.MangaModel;
 import com.manga.server.features.manga.responses.MangaResponse;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.springframework.stereotype.Service;
 
+@Service
 @Mapper(componentModel = "spring")
 public interface MangaMapper {
-
-  List<MangaResponse> mangasToMangaDTOs(List<MangaModel> mangas);
-
-  @Mapping(target = "thumbnail", source = "manga.thumbnail.fullUrl")
-  MangaResponse mangaToMangaDTO(MangaModel manga);
-
+    @Mapping(target = "thumbnail", source = "manga.thumbnail.fullUrl")
+    MangaResponse mangaToMangaResponse(MangaModel manga);
 }
