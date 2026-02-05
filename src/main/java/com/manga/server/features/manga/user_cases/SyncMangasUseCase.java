6b8f6aeeba6269ef0477f1ee221138161da7d075
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-05 18:15:00 +0100

    feat(manga): add SyncMangasUseCase for synchronizing mangas with new chapters

---
+++ src/main/java/com/manga/server/features/manga/user_cases/SyncMangasUseCase.java
@@ -0,0 +1,18 @@
+package com.manga.server.features.manga.user_cases;
+
+import com.manga.server.features.scrapper.services.ScrapperService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SyncMangasUseCase {
+
+    private ScrapperService scrapperService;
+    private SaveMangasUseCase saveMangasUseCase;
+
+    public void execute() {
+    var mangasWhitNewChapter = scrapperService.getMangasWithNewChapters();
+    saveMangasUseCase.execute(mangasWhitNewChapter);
+    }
+}
