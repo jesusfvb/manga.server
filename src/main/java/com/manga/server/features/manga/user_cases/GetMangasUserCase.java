26095e0d36027044c7444b4674cbaf4dc01911a2
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-06 18:21:07 +0100

    refactor(manga): rename validMangaListSize to sizeLestThat for clarity and update logic

--- src/main/java/com/manga/server/features/manga/user_cases/GetMangasUserCase.java
+++ src/main/java/com/manga/server/features/manga/user_cases/GetMangasUserCase.java
@@ -3,7 +3,6 @@ package com.manga.server.features.manga.user_cases;
 import com.manga.server.features.manga.controller.query.MangaQuery;
 import com.manga.server.features.manga.model.MangaModel;
 import com.manga.server.features.manga.repository.getmangas.GetMangaRepository;
-import com.manga.server.features.scrapper.services.ScrapperService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -20,7 +19,7 @@ public class GetMangasUserCase {
 
         var mangas = mangaRepository.findAll(query, pageable);
 
-        if(validSearch(query.getSearch() )&& validMangaListSize(mangas) ) {
+        if(validSearch(query.getSearch() )&& sizeLestThat(mangas) ) {
             return searchMangaUserCase.execute(query.getSearch(), mangas.getContent(), pageable);
         }
 
@@ -32,9 +31,9 @@ public class GetMangasUserCase {
         return search != null && !search.trim().isEmpty();
     }
 
-    private boolean validMangaListSize(Page<MangaModel> mangas) {
+    private boolean sizeLestThat(Page<MangaModel> mangas) {
         if (mangas == null) return false;
         mangas.getContent();
-        return !mangas.getContent().isEmpty() && mangas.getContent().size() <= 5;
+        return mangas.getContent().isEmpty() ||  mangas.getTotalElements() <= 5;
     }
 }
