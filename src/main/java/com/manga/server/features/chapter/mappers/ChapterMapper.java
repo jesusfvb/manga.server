dc9fe7842c534390945ac07b1a1636eeda51e98b
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-10 18:35:12 +0100

    refactor(chapter): Update ChapterMapper to use ChapterResponse instead of ChapterDTO

--- src/main/java/com/manga/server/features/chapter/mappers/ChapterMapper.java
+++ src/main/java/com/manga/server/features/chapter/mappers/ChapterMapper.java
@@ -2,17 +2,17 @@ package com.manga.server.features.chapter.mappers;
 
 import java.util.List;
 
+import com.manga.server.features.chapter.responses.ChapterResponse;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Service;
 
-import com.manga.server.features.chapter.responses.ChapterDTO;
 import com.manga.server.features.chapter.models.ChapterModel;
 
 @Service
 @Mapper(componentModel = "spring")
 public interface ChapterMapper {
 
-    ChapterDTO chapterToChapterDTO(ChapterModel chapter);
+    ChapterResponse chapterToChapterDTO(ChapterModel chapter);
 
-    List<ChapterDTO> chaptersToChapterDTOs(List<ChapterModel> chapters);
+    List<ChapterResponse> chaptersToChapterDTOs(List<ChapterModel> chapters);
 }
