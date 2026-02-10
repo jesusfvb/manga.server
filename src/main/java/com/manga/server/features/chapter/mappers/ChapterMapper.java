c4ffd8eb5fe9958040bd231189c3e54a35be01b0
Jesús Francisco Vázquez Biltre <jfvazquezbiltre@gmail.com>
2026-02-10 21:09:08 +0100

    refactor(chapter): rename mapping methods in ChapterMapper for consistency

--- src/main/java/com/manga/server/features/chapter/mappers/ChapterMapper.java
+++ src/main/java/com/manga/server/features/chapter/mappers/ChapterMapper.java
@@ -1,18 +1,14 @@
 package com.manga.server.features.chapter.mappers;
 
-import java.util.List;
-
-import com.manga.server.features.chapter.responses.ChapterResponse;
 import org.mapstruct.Mapper;
 import org.springframework.stereotype.Service;
 
 import com.manga.server.features.chapter.models.ChapterModel;
+import com.manga.server.features.chapter.responses.ChapterResponse;
 
 @Service
 @Mapper(componentModel = "spring")
 public interface ChapterMapper {
 
-    ChapterResponse chapterToChapterDTO(ChapterModel chapter);
-
-    List<ChapterResponse> chaptersToChapterDTOs(List<ChapterModel> chapters);
+    ChapterResponse toChapterResponse(ChapterModel chapter);
 }
